c3381c763779d8d56ba1ae94c4490c4623eaa215
David Palacat <dpalacat@uci.edu>
2024-02-12 23:40:41 -0800

    testing extractLinks

--- scraper.py
+++ scraper.py
@@ -50,6 +50,8 @@ def is_valid(url):
                 + r"|thmx|mso|arff|rtf|jar|csv"
                 + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower()):
             return False
+        else:
+            return True
     except TypeError:
         print("TypeError for ", parsed)
         raise
